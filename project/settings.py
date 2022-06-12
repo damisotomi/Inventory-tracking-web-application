33702391a5b6ae557cb1c7e08e6678c29fe687ac
damisotomi <sotomidamilola@gmail.com>
2022-06-12 15:59:48 +0100

    Ajusted setting to True to see if error occurs during deplpoyment

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p(!yyis_tx0e6__8on-%=0ojrk5@evvmqz6+b(jyaf@swje+a)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['dami-sotomi-shopify-backend-challenge.azurewebsites.net','127.0.0.1']
 
